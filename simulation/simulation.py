1ac38e228bc76a3c716427d75d2f676708c1c9a9
Gosan <1samuraisan1@gmail.com>
2026-06-13 19:25:03 +0300

    Trail version

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,4 +1,3 @@
-from pickle import POP
 import random 
 #переносим нашего агента из файла agent
 from agent import Agent 
@@ -41,5 +40,33 @@ class EvolutionSimulation:
         #новый список для следующего поколения
         new_agents = []
 
-        #
+        #Заполение сипска потомством до 50 
+        while len(new_agents) < self.population_size:
+            #выбираем двух случайных из лучших 
+            p_agent = random.choice(parents)
+            m_agent = random.choice(parents)
+
+            #создаем приемника 
+            child = Agnet (self.start_x, self.start_y)
+
+            #скрещиваем гены (половина от p_agent, половина от m_agent)
+            seprator = len(child.genome) // 2
+            child.genome = p_agent.genome[:seprator] + m_agent.genome[seprator:]
+
+            #смешение разных геномов *мутация
+            #пройдемся по всему геному и меняем команду на случайную
+            for i in range (len(child.genome)):
+                random_percent = random.random()
+                if random_percent < mutation_rate:
+                    child.genome[i] = random = random.randint(0, 3)
+
+                #добавляем потомка в новое поколение
+            new_agents.append(child)
+
+        #замена предыдущего поколения на новое 
+        self.agents = new_agents
+
+        #увеличиваем номер поколения +1
+        self.generation = self.generation + 1
+
         
